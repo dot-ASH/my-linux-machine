bf01e536258d948b45048f33536686b0db3c662f
dot-ash <sakir.ashker@gmail.com>
2024-11-15 02:31:50 +0600

    push

--- qtile/dotfiles/qtile/autostart_once.sh
+++ qtile/dotfiles/qtile/autostart_once.sh
@@ -6,5 +6,7 @@ wal -i ~/Wallpaper/claudio-testa-FrlCwXwbwkk-unsplash.jpg &&
 # Start picom
 picom --config ~/.config/picom/picom.conf &
 
+xrandr --output HDMI-0 --mode 1920x1080 --rate 74.97
+
 # Start Copyq
 copyq &
